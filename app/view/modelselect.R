c9070d5f7adac34897f4a2e8737a81697363413a
davidrsch <daviddrsch@gmail.com>
2023-10-01 02:27:13 +0200

    improving code's quality

--- app/view/modelselect.R
+++ app/view/modelselect.R
@@ -120,7 +120,7 @@ server <- function(
       # Getting start and end date from input
       startD <- ceiling_date(ymd(input$daterange1[1]), "month") - days(1)
       endD <- ceiling_date(ymd(input$daterange1[2]), "month") - days(1)
-  
+
       # Getting data from main and extracting results and indicators
       data <- get(paste0("returnsdata", input$modelselect))
       datar <- returns_data(data, startD, endD)
@@ -131,38 +131,37 @@ server <- function(
 
         fig <- plot_ly(datar, type = "scatter", mode = "lines")
         fig <- fig |>
-        # Lineas de RNA1
-        # Linea de valos máximos de RNA1
-        add_lines(x = ~Date, y = ~max_y, name = "RNA",
-                  line = list(color = "blue"), legendgroup = "struct1") |>
-        add_lines(x = ~Date, y = ~max_95, name = "max95",
-                  line = list(color = "blue"), fill = "tonexty", 
-                  fillcolor = "rgba(18,18,255,0.2)", legendgroup = "struct1",
-                  showlegend = F) |>
-        add_lines(x = ~Date, y = ~min_5, name = "min5",
-                  line = list(color = "blue"), fill = "tonexty",
-                  fillcolor = "rgba(18,18,255,0.5)", legendgroup = "struct1",
-                  showlegend = F) |>
-        add_lines(x = ~Date, y = ~min_y, name = "min",
-                  line = list(color = "blue"), fill = "tonexty",
-                  fillcolor ="rgba(18,18,255,0.2)", legendgroup = "struct1",
-                  showlegend = F) |>
-        add_lines(x = ~Date, y = ~meanPortre, name = "meanP",
-                  line= list(color = "blue",dash = "dash"),
-                  legendgroup = "struct1", showlegend = F) |>
-        add_lines(x = ~Date, y = ~IBEX, name = "IBEX",
-                  line = list(color="red")) |>
-        add_lines(x = ~Date, y = ~Means, name = "Means",
-                  line = list(color="green")) |>
-        layout(title = paste0("Carteras Estructura-", input$modelselect),
-               showlegend = T,
-               legend = list(y = 0.5),
-               xaxis = list(title="Fecha"),
-               yaxis = list(title="Valores"))
+          # Lineas de RNA1
+          # Linea de valos máximos de RNA1
+          add_lines(x = ~Date, y = ~max_y, name = "RNA",
+                    line = list(color = "blue"), legendgroup = "struct1") |>
+          add_lines(x = ~Date, y = ~max_95, name = "max95",
+                    line = list(color = "blue"), fill = "tonexty", 
+                    fillcolor = "rgba(18,18,255,0.2)", legendgroup = "struct1",
+                    showlegend = FALSE) |>
+          add_lines(x = ~Date, y = ~min_5, name = "min5",
+                    line = list(color = "blue"), fill = "tonexty",
+                    fillcolor = "rgba(18,18,255,0.5)", legendgroup = "struct1",
+                    showlegend = FALSE) |>
+          add_lines(x = ~Date, y = ~min_y, name = "min",
+                    line = list(color = "blue"), fill = "tonexty",
+                    fillcolor = "rgba(18,18,255,0.2)", legendgroup = "struct1",
+                    showlegend = FASE) |>
+          add_lines(x = ~Date, y = ~meanPortre, name = "meanP",
+                    line = list(color = "blue",dash = "dash"),
+                    legendgroup = "struct1", showlegend = FALSE) |>
+          add_lines(x = ~Date, y = ~IBEX, name = "IBEX",
+                    line = list(color = "red")) |>
+          add_lines(x = ~Date, y = ~Means, name = "Means",
+                    line = list(color = "green")) |>
+          layout(title = paste0("Carteras Estructura-", input$modelselect),
+                 showlegend = TRUE,
+                 legend = list(y = 0.5),
+                 xaxis = list(title = "Fecha"),
+                 yaxis = list(title = "Valores"))
 
         # Show the plot
-        fig
-        })
+        fig})
 
       # Generating output of indicators computed from obtained predictions
       output$indicatorschart <- plotly::renderPlotly({
@@ -170,11 +169,11 @@ server <- function(
         fig <- plot_ly(datai, type = "scatter", mode = "lines")
         fig <- fig |>
           add_lines(x = ~Date, y = ~meanmse, name = "MSE",
-                   line = list(color="blue")) |>
-          add_lines(x = ~Date, y = ~meanrsqrd, name ="R2",
-                    line = list(color="green")) |>
+                   line = list(color = "blue")) |>
+          add_lines(x = ~Date, y = ~meanrsqrd, name = "R2",
+                    line = list(color = "green")) |>
           layout(title = paste0("Indicadores Estructura-", input$modelselect),
-                 showlegend = T,
+                 showlegend = TRUE,
                  legend = list(y = 0.5),
                  xaxis = list(title = "Fecha"),
                  yaxis = list(title = "Valores"))
--- app/view/welcome.R
+++ app/view/welcome.R
@@ -8,9 +8,9 @@ box::use(
 
 #' @export
 server <- function(id) {
-  
+
   moduleServer(id, function(input, output, session) {
-    
+
     # Defining welcome message
     welcome <- modalDialog(
       title = div(
@@ -29,24 +29,24 @@ server <- function(id) {
       ),
       div(
         p(
-        'Este dashboard es parte del proyecto presentado como Trabajo Final de Máster en la 
-        Facultad de Economía y Empresas de la Universidad de A Coruña para la obtención del 
-        titúlo de Máster Universitario en Banca y Finanzas.'),
+          'Este dashboard es parte del proyecto presentado como Trabajo Final de Máster en la 
+          Facultad de Economía y Empresas de la Universidad de A Coruña para la obtención del 
+          titúlo de Máster Universitario en Banca y Finanzas.'),
         p(
-        'El proyecto se titula "Aplicación de redes neuronales artificiales y programación 
-        cuadrática en la gestión de carteras". El objetivo del trabajo se examina el potencial 
-        de la combinación de las redes neuronales convolucionales y LSTM para mejorar la 
-        previsión de series de tiempo y en el proceso de composición de carteras, se aplica la 
-        programación cuadrática como una técnica eficiente para lograr una distribución óptima 
-        de activos financieros. En conclusión, el enfoque de combinar redes neuronales 
-        artificiales y programación cuadrática muestra promesa en la gestión de carteras 
-        financieras, pero es necesario un estudio más profundo y exhaustivo para determinar su 
-        eficiencia óptima. Este trabajo sienta las bases para futuras investigaciones, 
-        destacando la importancia de utilizar datos actualizados y configurar adecuadamente los 
-        modelos para lograr una gestión de carteras más informada y efectiva en un entorno 
-        financiero en constante evolución.')
+          'El proyecto se titula "Aplicación de redes neuronales artificiales y programación 
+          cuadrática en la gestión de carteras". El objetivo del trabajo se examina el potencial 
+          de la combinación de las redes neuronales convolucionales y LSTM para mejorar la 
+          previsión de series de tiempo y en el proceso de composición de carteras, se aplica la 
+          programación cuadrática como una técnica eficiente para lograr una distribución óptima 
+          de activos financieros. En conclusión, el enfoque de combinar redes neuronales 
+          artificiales y programación cuadrática muestra promesa en la gestión de carteras 
+          financieras, pero es necesario un estudio más profundo y exhaustivo para determinar su 
+          eficiencia óptima. Este trabajo sienta las bases para futuras investigaciones, 
+          destacando la importancia de utilizar datos actualizados y configurar adecuadamente los 
+          modelos para lograr una gestión de carteras más informada y efectiva en un entorno 
+          financiero en constante evolución.')
         ),
-      easyClose = T,
+      easyClose = TRUE,
       footer = div("Creado por: David Díaz Rodríguez", style = "text-align:center;")
     )
     
